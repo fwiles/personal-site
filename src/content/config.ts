1fa4517d7da28982a9c62dd54b528e5517b8e56f
Fransisco <fransiscowiles@gmail.com>
2026-02-08 20:40:22 -0500

    articles overview page + articles page progress

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,4 +8,27 @@ const policies = defineCollection({
   }),
 });
 
-export const collections = { policies };
+const articles = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    tags: z.array(z.string()),
+    heroImage: z.string(),
+    author: z.object({
+      name: z.string(),
+      title: z.string(),
+      bio: z.string(),
+      avatar: z.string().optional(),
+      socials: z.array(
+        z.object({
+          label: z.string(),
+          url: z.string().url(),
+        })
+      ),
+    }),
+  }),
+});
+
+export const collections = { policies, articles };
