787c578b150c67af45e4f0e3a6c4d0c1c6afa5b3
Fransisco <fransiscowiles@gmail.com>
2026-02-16 20:28:56 -0500

    added smaller images to index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import { EXCLUDED_SITEMAP_PATHS } from './src/sitemap-exclude';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fransiscowiles.com',
+
   integrations: [
     sitemap({
       filter: (page) => !EXCLUDED_SITEMAP_PATHS.has(page),
     }),
   ],
+
+  adapter: cloudflare(),
 });
